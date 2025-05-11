3fa7d99a2f6729b022fd28c237500995066a8daa
Harish Balasubramanian <90543852+harish043@users.noreply.github.com>
2025-05-11 17:32:06 +0530

    Initial commit: Upload Tarantino React app

---
+++ src/components/Watchlist.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { MovieCard } from "./MovieCard";
+
+export const Watchlist = () => {
+  const { watchlist } = useContext(GlobalContext);
+
+  return (
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+          <h1 className="heading">Movies I need to watch...</h1>
+
+          <span className="count-pill">
+            {watchlist.length} {watchlist.length === 1 ? "Movie" : "Movies"}
+          </span>
+        </div>
+
+        {watchlist.length > 0 ? (
+          <div className="movie-grid">
+            {watchlist.map((movie) => (
+              <MovieCard movie={movie} key={movie.id} type="watchlist" />
+            ))}
+          </div>
+        ) : (
+          <h2 className="no-movies">“It's not what a movie is about, it's how it is about it.” - Roger Ebert</h2>
+        )}
+      </div>
+    </div>
+  );
+};
